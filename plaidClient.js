53ef9787d0bd72f58ed565376809d897aecfd41b
AndrewMLaValley <andy_seal5@yahoo.com>
2026-09-24 10:27:04 -0600

    Refactor plaidClient.js for consistent formatting

--- plaidClient.js
+++ plaidClient.js
@@ -1,17 +1,17 @@
 const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
 
-   const plaidEnv = process.env.PLAID_ENV || 'sandbox';
+const plaidEnv = process.env.PLAID_ENV || 'sandbox';
 
-   const config = new Configuration({
-     basePath: PlaidEnvironments[plaidEnv],
-     baseOptions: {
-       headers: {
-         'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
-         'PLAID-SECRET': process.env.PLAID_SECRET,
-       },
-     },
-   });
+const config = new Configuration({
+  basePath: PlaidEnvironments[plaidEnv],
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+});
 
-   const plaidClient = new PlaidApi(config);
+const plaidClient = new PlaidApi(config);
 
-   module.exports = { plaidClient };
+module.exports = { plaidClient };
